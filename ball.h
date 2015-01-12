f5a18c08575e31f63ee8af6af20ff49e49567522
Haroun Habeeb <kesari.cse.iitd.ac.in>
2015-01-12 14:46:08 +0530

    First commit! woo!

---
+++ ball.h
@@ -0,0 +1,20 @@
+/**
+	The main loop will create an array of balls which will handle their respective graphics and update their own positions etc. The main loop will merely handle the checking for collisions.
+*/
+class Ball {
+	private:
+		float xCentre,yCentre, radius;
+		float xVelocity, yVelocity;
+		float color[4];
+		float mass;
+	public:
+			Ball(float _xCentre , float _yCentre , float radius)  {
+				xCentre = _xCentre;
+				yCentre = _yCentre;
+				color[0] = 1.0;
+				color[1] = 0.0;
+				color[2] = 0.0;
+				mass = pow(radius,3); 
+			}
+			~Ball();
+}
---
+++ screenSaver.h
@@ -0,0 +1,14 @@
+/**
+	
+*/
+
+class ScreenSaver {
+private:
+	int numThreads;
+public:
+	ScreenSaver(int n);
+
+
+
+	void execute();
+}
