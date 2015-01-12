05e05ca3efb9391370e33d5a041c7e49782d08ab
Haroun Habeeb <kesari.cse.iitd.ac.in>
2015-01-12 17:39:51 +0530

    Some changes made

--- screenSaver.cpp
+++ screenSaver.cpp
@@ -1,5 +1,33 @@
 #ifndef SCREEN_SAVER_CPP
 	#define SCREEN_SAVER_CPP
 
-//CODE GOES HERE
+#include <screenSaver.h>
+
+/// function to initialize views, camera etc for glut.
+void screenSaver::init(Table* table, vector<Ball*>& ballList , int& numThreads) {
+	
+	glEnable(GL_DEPTH_TEST); //Ensure that 3d figures are drawn in the correct order.
+	glCullFace(GL_BACK); //Ensures that when a solid is drawn, the back figuyres arent draw. Thats a 2x improvmenet in performance.
+	glEnable(GL_CULL_FACE);
+
+
+	//R*R*R space to camera space setup.
+	glMatrixMode(GL_PROJECTION);
+	glLoadIDentity();
+	gluPerspective( 45 , //FOV in yz plane.
+			(float)WIDTH/(float)HEIGHT , //ratio
+			0.1f , //Near clipping distance
+			10000000f //Far clipping distance.
+		)
+	glMatrixMode(GL_MODELVIEW); // Object space to R*R*R space 
+	glLoadIdentity();
+
+	///Set background to brown.
+	glCLearColor( 0.54 , 0.49 , 0.41 , 1.0); 
+}
+
+
+void screenSaver::execute() {
+
+}
 #endif
--- screenSaver.h
+++ screenSaver.h
@@ -8,11 +8,13 @@
 
 class ScreenSaver {
 private:
-	int numThreads;
-	void init();
-	void reshape(int w , int h);
-	void keyHandler(unsigned char key, int x , int y);
-	void display();
+
+	int numThreads; ///Contains the number of threads requested by the make command.
+	void init(); /// function to initialize views, camera etc for glut.
+	void reshape(int w , int h); ///function to handle reshaping of windows
+	void keyHandler(unsigned char key, int x , int y); ///function to handle the keys pressed by the user - including , escape, spacebar and F
+	void display(); ///function to handle the actual display jobs.
+
 public:
 	ScreenSaver(int n) {
 		numThreads = n;
